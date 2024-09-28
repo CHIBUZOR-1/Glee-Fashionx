5cd1f77d8322b3841500064cd95d9d47e90648b6
Chibuzor Amaechi <amaechihenrychibuzor@gmail.com>
2024-09-28 06:58:18 +0100

    fixes

--- backend/index.js
+++ backend/index.js
@@ -45,10 +45,10 @@ app.use('/api/orders', orderRouter);
 app.use("/images", express.static('uploads'));
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
 
     app.get('*', (req, res)=> {
-        res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
 
